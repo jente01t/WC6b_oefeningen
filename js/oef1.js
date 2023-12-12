dbb61eba54a4a7ffab5a86adb451852c6a10f398
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-12-12 20:18:58 +0100

    Update oef1.js

--- js/oef1.js
+++ js/oef1.js
@@ -2,6 +2,10 @@
 
 // console.log(h2);
 
-let h2 = document.querySelector('#titel2');
+// let h2 = document.querySelector('#titel2');
 
-console.log(h2);
+// console.log(h2);
+
+let paragrafen = document.querySelectorAll('p');
+
+console.log(paragrafen);
