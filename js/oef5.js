107a812a43034ca7bfb9d85de8b3ff8cd666d246
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-12-13 09:54:40 +0100

    update main

---
+++ js/oef5.js
@@ -0,0 +1,14 @@
+let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+let letter = document.getElementById('letters');
+
+for (let i = 0; i < alphabet.length; i++) {
+    let txt = document.createTextNode(alphabet[i]);
+
+    let div = document.createElement('div');
+
+    div.appendChild(txt);
+
+
+    letter.appendChild(div);
+}
